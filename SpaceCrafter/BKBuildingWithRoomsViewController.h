f2f93c5f8edfc2a43bdf5dd9d5b02d22e659f968
Bykhkalo.Konstantyn <byhkalo.konstantyh@gmail.com>
2015-04-24 05:17:38 +0300

    New Room View Controller (error with segue)

---
+++ SpaceCrafter/BKBuildingWithRoomsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BKBuildingWithRoomsViewController.h
+//  SpaceCrafter
+//
+//  Created by Konstantyn Bykhkalo on 24.04.15.
+//  Copyright (c) 2015 Bykhkalo Konstantyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BKBuildingWithRoomsViewController : UIViewController
+
+@end
